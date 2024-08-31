54616192455061dffff1352ed504764a4935eaf6
Voletr <2448273134@qq.com>
2024-08-31 22:15:50 +0800

    初始化

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,7 +1,8 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import vue from '@vitejs/plugin-vue'
-
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 export default defineConfig({
   main: {
     plugins: [externalizeDepsPlugin()]
@@ -15,6 +16,11 @@ export default defineConfig({
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [vue()]
-  }
+    plugins: [vue()],
+    css: {
+      postcss: {
+        plugins: [tailwindcss, autoprefixer]
+      }
+    }
+  },
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/rederer/index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./src/rederer/index.html','./src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
